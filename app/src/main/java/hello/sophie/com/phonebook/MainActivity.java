06df6c0fc100d66697c02b0fba3c469227143357
sophiechoi <woojungc@gmail.com>
2015-05-19 17:29:01 +0900

    onclick, inserting names works

--- app/src/main/java/hello/sophie/com/phonebook/MainActivity.java
+++ app/src/main/java/hello/sophie/com/phonebook/MainActivity.java
@@ -4,16 +4,58 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+    private ListView listview;
+    private EditText editText;
+    private Button button;
+
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listview = (ListView) findViewById(R.id.main_listview);
+        editText = (EditText) findViewById(R.id.main_edittext);
+        button = (Button) findViewById(R.id.main_button);
+        button.setOnClickListener(this);
+
+        //listview adapter arrayList
+        //http://blog.naver.com/aro004/80203876954
+
+        list = new ArrayList<String>();
+        list.add("우정");
+        list.add("광");
+        list.add("kyu");
+
+        //this: 현재 액티비티
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+        listview.setAdapter(adapter);
     }
 
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+
+        switch (id) {
+            case R.id.main_button :
+                String input = editText.getText().toString();
+                list.add(input);
+                adapter.notifyDataSetChanged();
+                break;
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
